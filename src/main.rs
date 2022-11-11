565b3c058651d7873591ffb88c7137034db71924
habospace <avatarokwow@gmail.com>
2022-11-11 11:14:22 +0000

    use BTreeMap for storing brainfuck commands (because of dynamic sizing, ordering and quick lookup), add functionality to translate brainfuck code from string to list of brainfuck commands.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use std::collections::LinkedList;
 use std::collections::HashMap;
+use std::collections::BTreeMap;
 
+#[derive(Copy, Clone)]
 enum BfCommand {
     IncrPointer,
     DecrPointer,
@@ -8,12 +10,15 @@ enum BfCommand {
     Decr,
     Print,
     BlockStart,
-    BlockEnd
+    BlockEnd,
+    Comment
 }
 
-// TODO: rewrite this with a type that supports runtime sizing
-// Array only support compile time static sizing
-type BfCode = [BfCommand;106];
+type Index = usize;
+type BlockStartIndex = usize;
+type BlockEndIndex = usize;
+
+type BfCode = BTreeMap<Index, BfCommand>;
 
 struct BfMemory<'a> {
     right:  &'a mut LinkedList<i32>,
@@ -22,13 +27,35 @@ struct BfMemory<'a> {
 }
 
 struct BfProgram<'a> {
-    code: Box<BfCode>,
+    code: BfCode,
     memory: &'a mut BfMemory<'a>,
 }
 
 struct BfProgramExecution<'a> {
-    print_chars: &'a mut LinkedList<String>, 
+    print_chars: &'a mut LinkedList<char>,
     program:     &'a mut BfProgram<'a>
+}
+
+fn char_to_bf_command(c: char) -> BfCommand {
+    let command: BfCommand = match c {
+        '>' => BfCommand::IncrPointer,
+        '<' => BfCommand::DecrPointer,
+        '+' => BfCommand::Incr,
+        '-' => BfCommand::Decr,
+        '.' => BfCommand::Print,
+        '[' => BfCommand::BlockStart,
+        ']' => BfCommand::BlockEnd,
+        _   => BfCommand::Comment
+    };
+    command
+}
+
+fn translate_string_to_bf_code(str_code: &String) -> BfCode {
+    let mut bfcode: BfCode = BTreeMap::new();
+    for (i, char_command) in str_code.chars().enumerate() {
+        bfcode.insert(i, char_to_bf_command(char_command));
+    }
+    bfcode
 }
 
 fn move_cursor_right(memory: &mut BfMemory) {
@@ -62,34 +89,36 @@ fn decr_val(memory: &mut BfMemory) {
     *memory.middle -= 1;
 }
 
-fn to_ascii(i: &i32) -> String {
+fn to_ascii(i: &i32) -> char {
     match *i {
-        x@0..=127 => format!("{:?}", x as u8 as char),
-        _ => "".into(),
+        x@0..=127 => x as u8 as char,
+        _ => ' ',
     }
 }
 
-fn extract_blocks(code: &Box<BfCode>) -> (HashMap<usize, usize>, HashMap<usize, usize>) {
-    let mut block_start_end_map: HashMap<usize, Option<usize>> = HashMap::new();
-    let mut block_starts: LinkedList<usize> = LinkedList::new();
-    for (i, command) in code.iter().enumerate() {
+fn extract_blocks(code: &BfCode) ->
+    (HashMap<BlockEndIndex, BlockStartIndex>, HashMap<BlockStartIndex, BlockEndIndex>) {
+
+    let mut block_start_end_map: HashMap<BlockStartIndex, Option<BlockEndIndex>> = HashMap::new();
+    let mut block_starts: LinkedList<BlockStartIndex> = LinkedList::new();
+    for (i, command) in code {
         match command {
             BfCommand::BlockStart => {
-                block_start_end_map.insert(i, None);
-                block_starts.push_back(i);
+                block_start_end_map.insert(*i, None);
+                block_starts.push_back(*i);
 
             }
             BfCommand::BlockEnd   => {
                 match block_starts.pop_back() {
                     None              => {panic!("Unmatched block end (']') at {i}.");}
-                    Some(block_start) => {block_start_end_map.insert(block_start, Some(i));}
+                    Some(block_start) => {block_start_end_map.insert(block_start, Some(*i));}
                 }
             }
             _                     => ()
         }
     }
-    let mut block_end_start_map: HashMap<usize, usize> = HashMap::new();
-    let mut block_start_end_map_: HashMap<usize, usize> = HashMap::new();
+    let mut block_end_start_map: HashMap<BlockEndIndex, BlockStartIndex> = HashMap::new();
+    let mut block_start_end_map_: HashMap<BlockStartIndex, BlockEndIndex> = HashMap::new();
     for (block_start, maybe_block_end) in &block_start_end_map {
         match maybe_block_end {
             None            => {panic!("Unmatched block start ('[') at {block_start}.");}
@@ -106,18 +135,21 @@ fn run_bf_program(execution: &mut BfProgramExecution) {
     let mut i: usize = 0;
     let (block_ends_starts, block_starts_ends_map) = extract_blocks(&execution.program.code);
     while i < execution.program.code.len() {
-        let command = &execution.program.code[i];
+        let command = match execution.program.code.get(&i) {
+            None    => BfCommand::Comment,
+            Some(c) => *c
+        };
         match command {
             BfCommand::IncrPointer => {move_cursor_right(execution.program.memory); i+=1;}
             BfCommand::DecrPointer => {move_cursor_left(execution.program.memory); i+=1;}
             BfCommand::Incr        => {incr_val(execution.program.memory); i+=1;}
             BfCommand::Decr        => {decr_val(execution.program.memory); i+=1;}
             BfCommand::Print       => {
-                let print_char: String = to_ascii(execution.program.memory.middle);
+                let print_char: char = to_ascii(execution.program.memory.middle);
                 execution.print_chars.push_back(print_char);
                 i+=1;
             }
-            BfCommand::BlockStart => {
+            BfCommand::BlockStart  => {
                 if *execution.program.memory.middle == 0 {
                     match block_starts_ends_map.get(&i) {
                         None => (),
@@ -127,7 +159,7 @@ fn run_bf_program(execution: &mut BfProgramExecution) {
                     i+=1;
                 }
             }
-            BfCommand::BlockEnd   => {
+            BfCommand::BlockEnd    => {
                 if *execution.program.memory.middle == 0 {
                     i+=1;
                 } else {
@@ -137,32 +169,18 @@ fn run_bf_program(execution: &mut BfProgramExecution) {
                     }
                 }
             }
+            _                      => {i+=1;}
         }
     }
 }
 
 fn main() {
-    let hello_world_bf_code: BfCode = [
-        BfCommand::Incr, BfCommand::Incr, BfCommand::Incr, BfCommand::Incr, BfCommand::Incr, BfCommand::Incr, 
-        BfCommand::Incr, BfCommand::Incr, BfCommand::BlockStart, BfCommand::IncrPointer, BfCommand::Incr, BfCommand::Incr, 
-        BfCommand::Incr, BfCommand::Incr, BfCommand::BlockStart, BfCommand::IncrPointer, BfCommand::Incr, BfCommand::Incr, 
-        BfCommand::IncrPointer, BfCommand::Incr, BfCommand::Incr, BfCommand::Incr, BfCommand::IncrPointer, BfCommand::Incr, 
-        BfCommand::Incr, BfCommand::Incr, BfCommand::IncrPointer, BfCommand::Incr, BfCommand::DecrPointer, BfCommand::DecrPointer, 
-        BfCommand::DecrPointer, BfCommand::DecrPointer, BfCommand::Decr, BfCommand::BlockEnd, BfCommand::IncrPointer, BfCommand::Incr, 
-        BfCommand::IncrPointer, BfCommand::Incr, BfCommand::IncrPointer, BfCommand::Decr, BfCommand::IncrPointer, BfCommand::IncrPointer, 
-        BfCommand::Incr, BfCommand::BlockStart, BfCommand::DecrPointer, BfCommand::BlockEnd, BfCommand::DecrPointer, BfCommand::Decr, 
-        BfCommand::BlockEnd, BfCommand::IncrPointer, BfCommand::IncrPointer, BfCommand::Print, BfCommand::IncrPointer, BfCommand::Decr, 
-        BfCommand::Decr, BfCommand::Decr, BfCommand::Print, BfCommand::Incr, BfCommand::Incr, BfCommand::Incr, BfCommand::Incr, 
-        BfCommand::Incr, BfCommand::Incr, BfCommand::Incr, BfCommand::Print, BfCommand::Print, BfCommand::Incr, BfCommand::Incr, 
-        BfCommand::Incr, BfCommand::Print, BfCommand::IncrPointer, BfCommand::IncrPointer, BfCommand::Print, BfCommand::DecrPointer, 
-        BfCommand::Decr, BfCommand::Print, BfCommand::DecrPointer, BfCommand::Print, BfCommand::Incr, BfCommand::Incr, BfCommand::Incr, 
-        BfCommand::Print, BfCommand::Decr, BfCommand::Decr, BfCommand::Decr, BfCommand::Decr, BfCommand::Decr, BfCommand::Decr, 
-        BfCommand::Print, BfCommand::Decr, BfCommand::Decr, BfCommand::Decr, BfCommand::Decr, BfCommand::Decr, BfCommand::Decr, 
-        BfCommand::Decr, BfCommand::Decr, BfCommand::Print, BfCommand::IncrPointer, BfCommand::IncrPointer, BfCommand::Incr, BfCommand::Print,
-        BfCommand::IncrPointer, BfCommand::Incr, BfCommand::Incr, BfCommand::Print
-        
-    ];
-
+    let hello_world_bf_code_str: String = String::from(
+        "++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]
+        <-]>>.>---.+++++++..+++.>>.<-.<.+++.------.----
+        ----.>>+.>++."
+    );
+    let hello_world_bf_code: BfCode = translate_string_to_bf_code(&hello_world_bf_code_str);
     let mut print_chars = LinkedList::new();
     let mut left = LinkedList::from([0; 1000]);
     let mut middle = 0;
@@ -172,7 +190,7 @@ fn main() {
     };
     let mut bf_program = BfProgram {
         memory: &mut bf_memory,
-        code: Box::new(hello_world_bf_code)
+        code: hello_world_bf_code
     };
     let mut execution = BfProgramExecution {
         print_chars: &mut print_chars,
@@ -180,5 +198,10 @@ fn main() {
     };
 
     run_bf_program(&mut execution);
-    println!("{:?}", print_chars);
+
+    let mut print_string = String::new();
+    for c in print_chars {
+        print_string.push(c);
+    }
+    println!("{:?}", print_string);
 }
